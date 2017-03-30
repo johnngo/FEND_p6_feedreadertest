5539e764aa6b2dce64bb61adfa51cc9052549381
John <johnngo@Johns-MacBook-Pro.local>
2017-03-30 15:23:52 -0700

    the menu test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -33,7 +33,9 @@ $(function() {
          */
          it('has a URL defined and URL is not empty', function() {
             allFeeds.forEach(function(i){
+                // check to see if url is defined
                 expect(i.url).toBeDefined();
+                // check to see if url is empty
                 expect(i.url.length).not.toBe(0);
             });
         });
@@ -45,7 +47,9 @@ $(function() {
 
          it('has a name defined and name is not empty', function() {
             allFeeds.forEach(function(i){
+                //check to see if name is defined
                 expect(i.name).toBeDefined();
+                //check to see if name is empty
                 expect(i.name.length).not.toBe(0);
             });
         });
@@ -53,28 +57,39 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+        describe ('The menu',function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+         it('check that menu is hidden',function(){
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+         });
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+            it('menu hides and appears on menu icon click', function (){
+                //when menu icon is clicked, menu list appears, menu-hidden class changes
+                $('.menu-icon-link').click();
+                expect($('body').hasClass('menu-hidden')).toBe(false);
+                //when menu list appears, menu icon is clicked, menu-hidden class changes
+                $('.menu-icon-link').click();
+                expect($('body').hasClass('menu-hidden')).toBe(true);
+            })
+      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+        describe ('Initial Entries', function(){
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+            });
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
